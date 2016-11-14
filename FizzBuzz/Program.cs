5c517dbf548b5c13262d0eb287760022ea9d5a63
JH05 <JA.H@Outlook.com>
2016-11-14 16:36:07 +0000

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			for (int i = 1; i <= 100; i++)
+			{
+				if (i % 15 == 0)
+				{
+					Console.WriteLine("FizzBuzz");
+					continue;
+				}
+				if (i % 3 == 0)
+				{
+					Console.WriteLine("Fizz");
+					continue;
+				}
+				if (i % 5 == 0)
+				{
+					Console.WriteLine("Buzz");
+					continue;
+				}
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
